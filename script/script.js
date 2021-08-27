b3e04c45880cf56e11cf30cefb6566a7a0fd7172
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-27 23:17:28 +0300

    lesson18

--- script/script.js
+++ script/script.js
@@ -42,4 +42,69 @@ window.addEventListener('DOMContentLoaded', () => {
         updateClock();
     }
     const idInterval = setInterval(countTimer, 1000, '27 august 2021');
+
+
+    //menu
+
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = document.querySelectorAll('ul>li');
+
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+
+    toggleMenu();
+
+    //popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close'),
+            popUpContent = document.querySelector('.popup-content');
+            let count = 0;
+
+        popupBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+
+        });
+
+        let flyInterval;
+        let modalWindow = () => {
+            flyInterval = requestAnimationFrame(modalWindow);
+            count ++;
+            if (count < 200) {
+                popUpContent.style.left = count * 5 + 'px';
+            } else {
+                cancelAnimationFrame(flyInterval);
+            };
+            if (document.documentElement.clientWidth < 768) {
+                cancelAnimationFrame(flyInterval);
+            };
+        };
+
+        document.addEventListener('click', () => {
+             flyInterval = requestAnimationFrame(modalWindow);
+        });
+
+    };
+    togglePopUp();
+
 });
