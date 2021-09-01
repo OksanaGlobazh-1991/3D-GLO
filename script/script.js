51c87fec3b4783acbc72ac9eb561570bb5c400af
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-01 22:45:35 +0300

    lesson24

--- script/script.js
+++ script/script.js
@@ -268,34 +268,24 @@ window.addEventListener('DOMContentLoaded', () => {
     slider();
 
 
-    //наша команда(фото)
+    //валидация
 
     const commandPhoto = document.querySelectorAll('.command__photo');
-    
-            commandPhoto.forEach(item => {
-                let photo;
 
-                item.addEventListener('mouseover', event => {
-                    photo = event.target.src;
-                    event.target.src = event.target.dataset.img;
-                    console.log(item);
-                });
-                item.addEventListener('mouseout', event => {
-                    event.target.src = photo;
-                    console.log(item);
-                });
-            });
+    commandPhoto.forEach(item => {
+        let photo;
 
-            const calcBlock = document.querySelector('.calc-block');
-            const input = calcBlock.querySelectorAll('input');
-                
-                input.forEach(item => {
-                    item.addEventListener('input', () => {
-           
-                        item.value = item.value.replace(/[^0-9]/g, '');
-                    });
-                });
-    
+        item.addEventListener('mouseover', event => {
+            photo = event.target.src;
+            event.target.src = event.target.dataset.img;
+            console.log(item);
+        });
+        item.addEventListener('mouseout', event => {
+            event.target.src = photo;
+            console.log(item);
+        });
+    });
+
     const formName = document.getElementById('form2-name');
     formName.addEventListener('input', () => {
         formName.value = formName.value.replace(/^[а-яё -]+/g);
@@ -328,7 +318,7 @@ window.addEventListener('DOMContentLoaded', () => {
         formEmail.value = formEmail.value.trim();
         formEmail.value = formEmail.value.replace(/\s+/g, ' ');
     });
-    
+
 
     const formPhone = document.getElementById('form2-phone');
     formPhone.addEventListener('input', () => {
@@ -339,5 +329,62 @@ window.addEventListener('DOMContentLoaded', () => {
         formPhone.value = formPhone.value.trim();
         formPhone.value = formPhone.value.replace(/\s+/g, ' ');
     });
+
+    //калькулятор
+
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            input = calcBlock.querySelectorAll('input'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        input.forEach(item => {
+            item.addEventListener('input', () => {
+
+                item.value = item.value.replace(/[^0-9]/g, '');
+            });
+        });
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            if (target.matches('.calc-type') || target.matches('.calc-square') ||
+                target.matches('.calc-day') || target.matches('.calc-count')) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
+
 
 });
